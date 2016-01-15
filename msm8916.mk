a040110e0f53c7a707d3c00117a8de1607d6e642
Ethan Chen <intervigil@gmail.com>
2016-01-14 16:21:45 -0800

    msm8916: Remove unused items
    * These packages and binaries don't exist anymore
    
    Change-Id: I65ae1802d18f22eb441314bca8617e1e2d284161

--- msm8916.mk
+++ msm8916.mk
@@ -71,13 +71,6 @@ PRODUCT_PROPERTY_OVERRIDES +=
     persist.cne.feature=4
 endif
 
-# CRDA
-PRODUCT_PACKAGES += \
-    crda \
-    linville.key.pub.pem \
-    regdbdump \
-    regulatory.bin
-
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8916 \
@@ -85,11 +78,6 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8916 \
     libtinyxml \
     memtrack.msm8916
-
-# Filesystem
-PRODUCT_PACKAGES += \
-    e2fsck \
-    make_ext4fs
 
 # FM
 PRODUCT_PACKAGES += \
@@ -136,9 +124,7 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    init.crda.sh \
-    init.qcom.bt.sh \
-    init.qcom.coex.sh
+    init.qcom.bt.sh
 
 PRODUCT_PACKAGES += \
     init.qcom.rc \
@@ -155,10 +141,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
-
-# USB
-PRODUCT_PACKAGES += \
-    com.android.future.usb.accessory
 
 # Wifi
 PRODUCT_PACKAGES += \
