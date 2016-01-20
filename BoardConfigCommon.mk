d1e9d5a014c64ad73d7cff840d5423a2c16db4d2
Arvin Quilao <arquilao@gmail.com>
2016-01-20 13:55:15 +0800

    Revert changes to BoardConfigCommon.mk
    Change-Id: I70382c7fa40f01a81f731434bcc7d67b06cda02e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,10 +60,10 @@ TARGET_SYSTEM_PROP := $(VENDOR_PATH)/system.prop
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
-#BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
-#BOARD_DTBTOOL_ARGS := -2
+BOARD_DTBTOOL_ARGS := -2
 ifneq ($(FORCE_32_BIT),true)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
@@ -73,7 +73,7 @@ TARGET_USES_UNCOMPRESSED_KERNEL := true
 else
 TARGET_KERNEL_ARCH := arm
 endif
-#TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
+TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
